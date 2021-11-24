05eaaa486e6d7f8488f1ec0adffbcc68e9389f8d
NguyenNhatThao <51015626+NguyenNhatThao@users.noreply.github.com>
2021-11-24 13:08:26 +0700

    add

--- src/pages/StructuralPage.js
+++ src/pages/StructuralPage.js
@@ -2,12 +2,18 @@ import React , {useState , useEffect} from 'react'
 import Header from '../components/Header';
 import AddClassForm from '../components/AddClassForm';
 import ManageProfileForm from '../components/ManageProfileForm';
-import { Container } from '@mui/material';
+import { AccordionDetails, Container } from '@mui/material';
 import PopUp from '../components/Popup';
 
 import { useHistory, useParams } from 'react-router-dom'
 import axios from 'axios'
 import api from '../uri';
+import "../style/StructuralPage.css"
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import SaveIcon from '@material-ui/icons/Save';
+import Button from '@material-ui/core/Button';
+
+
 
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
@@ -22,26 +28,10 @@ const StructuralPage= ()=>{
     const [cls , setCls] = useState(null);
     const [isPopup, setisPopup] = useState(false)
     const [isPopupProfile, setisPopupProfile] = useState(false)
-    const [newGradeForm, setNewGradeForm] = useState(false);
-    // const [showGrade, setShowGrade]= useState(false);
-    const [listGrade, setListGrade]= useState([]);
-
-    useEffect(()=>{
-      axios.get(api+ `${id}/gradeAll`, {},
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': localStorage.getItem('Authorization'),
-        },
-      })
-      .then(response=>{
-        console.log(response);
-        setListGrade(response.data.data);
-      })
-      .catch(err=>{
-        console.log(err);
-      })
-    }, []);
+    const [listGrade, setListGrade]= useState([{
+      gradeText: "",
+      grade: ""
+    }]);
 
     useEffect(() => {
         axios.get(api+ `class/${id}/classDetail`)
@@ -73,44 +63,72 @@ const StructuralPage= ()=>{
     }
     
     function onSubmitProfileForm(data) {
-    axios.post(api +  'changeProfile' , data ,
-    {
-        headers: {
-        'Content-Type': 'application/json',
-        'Authorization': localStorage.getItem('Authorization'),
-        },
-    })
-    .then(response => {
-        setisPopupProfile(false);
-    }).catch(err => {
-        if (err.response.data.message === "User is not valid"){
-        alert("User is not valid") 
-        }
-        setisPopupProfile(false);
-    });;
+      axios.post(api +  'changeProfile' , data ,
+      {
+          headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('Authorization'),
+          },
+      })
+      .then(response => {
+          setisPopupProfile(false);
+      }).catch(err => {
+          if (err.response.data.message === "User is not valid"){
+          alert("User is not valid") 
+          }
+          setisPopupProfile(false);
+      });;
     }
 
-    const addGradeForm= (data)=>{
-        axios.post(api +  'grade/new' , {gradeName : data.gradeName, grade: data.grade, classId: cls._id} ,
+    const changeGradeText= (gradeText, i)=>{
+      let newGrade= [...listGrade];
+      newGrade[i].gradeText= gradeText;
+      setListGrade(newGrade);
+    }
+
+    const changeGrade= (grade, i)=>{
+      let newGrade= [...listGrade];
+      newGrade[i].grade= grade;
+      setListGrade(newGrade);
+    }
+
+    const addMoreGrade= ()=>{
+      setListGrade([...listGrade, {gradeText: "", grade: ""}]);
+    }
+
+    const gradeUI=()=>{
+      return listGrade.map((grade, i)=>(
+        <div className="gradeBox">
+          <AccordionDetails className="addGrade">
+            <div className="addGradeTop">
+              <input type="text" className="gradeText" placeholder="Name of component grade" value={grade.gradeText} onChange={e=>{changeGradeText(e.target.value, i)}}></input>
+              <input type="number" className="grade" placeholder="Grade" value={grade.grade} onChange={e=>{changeGrade(e.target.value, i)}}></input>
+            </div>
+          </AccordionDetails>
+          <AddCircleOutlineIcon onClick={addMoreGrade} className="edit"/>
+        </div>
+      ))
+    }
+
+    const saveForm= ()=>{
+      axios.post(api+ `class/${id}/newAssignment`, {fieldArray: listGrade, name: cls.className},
         {
           headers: {
             'Content-Type': 'application/json',
             'Authorization': localStorage.getItem('Authorization'),
           },
         })
-        .then(response => {
-          console.log(response.data);
-          setNewGradeForm(false);
-          // setShowGrade(true);
-        }).catch(err => {
+        .then(response=>{
+          console.log(response);
+          alert("Successful");
+        })
+        .catch(err => {
           if (err.response.data.message === "Student does not have permisson"){
             alert("Student does not have permisson") 
           }else {
             alert("Something wrong") 
           }
-          setNewGradeForm(false);
-        });;
-
+        });
     }
 
     return(
@@ -129,21 +147,16 @@ const StructuralPage= ()=>{
                     onManageProfile = {() => {setisPopupProfile(true)}}
                     onAddClassHandle={() => {setisPopup(true)}}>
                 </Header>}
-            <Container fixed> 
-              {cls &&
-                    <Tooltip title="Add Component Grade">
-                      <IconButton onClick={()=>setNewGradeForm(true)}>
-                          <AddIcon></AddIcon>
-                      </IconButton>
-                    </Tooltip>
-              }
-              {newGradeForm &&
-                  <AddGradeForm onSubmit= {addGradeForm}></AddGradeForm>
-              } 
-              {/* {showGrade &&
-                <GradeList title = "Grade of Course" list = {listGrade}></GradeList>
-              } */}
-              <GradeList title = "Grade of Course" list = {listGrade}></GradeList>
+            <Container fixed className= "form"> 
+              <div className="section">
+                <div className="title_section">
+                  <div className="form_top">
+                    <input type="text" className="form_top_name" style={{color: "black"}} placeholder="Untitled Document"></input>
+                  </div>
+                </div>
+              </div>
+              {gradeUI()}
+              <Button variant="contained" color="primary" onClick={saveForm} style={{fontSize:"14px"}}>Save</Button>
             </Container>
             
         </div>
