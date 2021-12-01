f9d6212a30d7bb512c763581ee1d90458214535e
Nguyen Le Hoang Nam <nam.nguyen@popsww.com>
2021-12-01 22:15:59 +0700

    Update uri.js

--- src/uri.js
+++ src/uri.js
@@ -1,3 +1,3 @@
-// const api = "https://midtermproject160220.herokuapp.com/api/";
-const api = "http://localhost:3000/api/";
+const api = "https://midtermproject160220.herokuapp.com/api/";
+// const api = "http://localhost:3000/api/";
 export default api;
