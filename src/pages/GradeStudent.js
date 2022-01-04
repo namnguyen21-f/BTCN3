85ecc39afa9fd81b8ba79f1907500fb742bc2a8c
NguyenNhatThao <51015626+NguyenNhatThao@users.noreply.github.com>
2022-01-04 10:48:31 +0700

    request a review

--- src/pages/GradeStudent.js
+++ src/pages/GradeStudent.js
@@ -3,12 +3,18 @@ import api from '../uri';
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import {Paper, Table, TableHead, TableRow, TableCell, TableBody} from '@mui/material';
+import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import { Typography, Box, TextField, Button, Grid } from '@mui/material';
+import PopUp from "../components/Popup";
 
 
 const GradeStudent= ()=>{
+    let grade
+    let message
     const { id } = useParams();
     const [cls, setCls]= useState(null)
     const [listGrade, setListGrade]= useState([])
+    const [popupRequest, setPopupRequest]= useState(false)
     useEffect(()=>{
         axios.get(api+ `class/${id}/classDetail`)
         .then(response=>{
@@ -45,6 +51,21 @@ const GradeStudent= ()=>{
         }
     }
 
+    const sendRequest= (assId)=>{
+        axios.post(api+ `/class/${id}/${assId}/review`,{grade: grade, text: message, studentId: listGrade.studentId},
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('Authorization'),
+            },
+        })
+        .then(response => {
+            setPopupRequest(false)
+        }).catch(err => {
+            alert(err.response.data.message);
+        });
+    }
+
     return (
         <div>
             {cls && 
@@ -54,7 +75,36 @@ const GradeStudent= ()=>{
                             <TableRow style={{textAlign: 'center'}}>
                                 <TableCell>Student Id</TableCell>
                                 {cls.assignmentList.map(ass=>(
-                                    <TableCell id={ass._id}>{ass.name}</TableCell>
+                                    <TableCell id={ass._id}>{ass.name}
+                                        <RequestQuoteIcon onClick={()=>setPopupRequest(true)} fontSize="small"></RequestQuoteIcon>
+                                        {popupRequest && 
+                                            <PopUp onClose={()=>{setPopupRequest(false)}}>
+                                                <Box component="form" noValidate autoComplete="off">
+                                                    <Typography variant="h4" component="div" color="black" style={{textAlign: "center", marginBottom: "1.5rem"}}>
+                                                        Request a review
+                                                    </Typography>
+                                                    <Grid container direction={"column"} spacing={3}>
+                                                        <Grid item>
+                                                            <TextField type="number" variant="standard" label="Grade" onChange={(e)=> {grade= e.target.value}}/>
+                                                        </Grid>
+                                                        <Grid item>
+                                                            <TextField
+                                                            id="standard-search"
+                                                            label="Explanation message"
+                                                            type="search"
+                                                            variant="standard"
+                                                            fullWidth
+                                                            onChange={(e)=> {message= e.target.value}}
+                                                            />
+                                                        </Grid>
+                                                        <Grid item>
+                                                            <Button variant="contained" onClick={()=> sendRequest(ass._id)}>Send</Button>
+                                                        </Grid>
+                                                    </Grid>
+                                                </Box>
+                                            </PopUp>}
+                                    </TableCell>
+
                                 )
                                 )}
                                 <TableCell>Total</TableCell>
